bbce488bc24d5ac9942d3b0ca8c4a61831acb806
Greg <cogh0020@algonquinlive.com>
2013-11-27 18:06:08 -0500

    Aw shiggity

--- TextBasedAdventureGame/Program.cs
+++ TextBasedAdventureGame/Program.cs
@@ -7,12 +7,39 @@ namespace TextBasedAdventureGame
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
             bool playing = true;
             Game theGame = new Game();
             theGame.reset();
+            Console.WriteLine("Fail Corp Presents....");
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine(" _______  _______ _________ _ ");
+            Console.WriteLine("|  ____ \\(  ___  )\\__   __/( \\");
+            Console.WriteLine("| (    \\/| (   ) |   ) (   | (");
+            Console.WriteLine("| (__    | (___) |   | |   | | ");
+            Console.WriteLine("|  __)   |  ___  |   | |   | |");
+            Console.WriteLine("| (      | (   ) |   | |   | |  ");
+            Console.WriteLine("| )      | )   ( |___) (___| (____/\\");
+            Console.WriteLine("|/       |/     \\|\\_______/(_______/");
+
+            System.Threading.Thread.Sleep(500);
+
+            Console.WriteLine(" _______           _______  _______ _________");
+            Console.WriteLine("(  ___  )|\\     /|(  ____ \\(  ____ \\__   __/");
+            Console.WriteLine("| (   ) || )   ( || (    \\/| (    \\/   ) (   ");
+            Console.WriteLine("| |   | || |   | || (__    | (_____    | |  ");
+            Console.WriteLine("| |   | || |   | ||  __)   (_____  )   | |  ");
+            Console.WriteLine("| | /\\| || |   | || (            ) |   | |  ");
+            Console.WriteLine("| (_\\ \\ || (___) || (____/\\/\\____) |   | |  ");
+            Console.WriteLine("(____\\/_)(_______)(_______/\\_______)   )_(  ");
+
+            System.Threading.Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine("Made by Joel Cright, Nikolai Morin Cull, Andy Lovett, and Greg Coghill");
+            System.Threading.Thread.Sleep(3000);
+            Console.Clear();
             while (playing == true)
             {
                 theGame.checkIfValid(Console.ReadLine());
@@ -43,6 +70,10 @@ namespace TextBasedAdventureGame
         {
             switch (toHere)
             {
+                case "Tavern":
+                    theTavern();
+                    break;
+
                 case "Water":
                     waterStuff();
                     break;
@@ -58,7 +89,26 @@ namespace TextBasedAdventureGame
                 case "Inventory":
                     printInventory();
                     break;
+
+                default:
+                    Console.WriteLine("That command is invalid. Please try again\n");
+                    break;
             }
+        }
+
+        private void gameOver()
+        {
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("You have died.");
+            System.Threading.Thread.Sleep(1000);
+            Console.Beep();
+            System.Threading.Thread.Sleep(1000);
+            Console.Beep();
+            System.Threading.Thread.Sleep(1000);
+            Console.Beep();
+            Console.Clear();
+            reset();
         }
 
         private void printInventory()
@@ -80,19 +130,16 @@ namespace TextBasedAdventureGame
                     inventory.Remove("Rope");
                     return;
                 }
-
                 else
                 {
                     continue;
                 }
             }
-
             if (beenHereWater == true)
             {
                 Console.WriteLine("Having been here already, you cockily attempt to traverse the rapids. Your quick steps lead to your downfall");
                 reset();
             }
-
             else
             {
                 Console.WriteLine("You gaze around in wonder. Having never seen such quick rapids before, so you take your time crossing. Unfortunately, taking your time means you were in the way when a tree came down, knocking you into the wtaer, where you freeze/drown/get clubbed by a tree to death.");
@@ -124,12 +171,82 @@ namespace TextBasedAdventureGame
                     inventory.Remove(thingToDrop);
                     inventory.TrimExcess();
                 }
-
                 else
                 {
                     continue;
                 }
             }
+        }
+
+        public void theTavern()
+        {
+            int broseph = 0;
+            bool inTavern = true;
+            //Required bools
+            //TODO: Item from drunk man
+            Console.WriteLine("You walk into a tavern. You see many patrons and wenches. You notice a large man at the bar\n");
+
+            while (inTavern == true)
+            {
+                Console.WriteLine("What will you do?\n");
+
+                Console.WriteLine("Fight Man");
+                Console.WriteLine("Talk to Man");
+                Console.WriteLine("Drink with Man");
+                Console.WriteLine("Leave");
+                Console.WriteLine(": ");
+                string input = Console.ReadLine();
+               
+
+                switch (input)
+                {
+                    case "Fight Man":
+                        {
+                            Console.WriteLine("You point to the man and challenge his manliness.\n He immediately gets up, brandishes an large axe, and removes your head from your shoulders.");
+                            System.Threading.Thread.Sleep(3000);
+                            inTavern = false;
+                            gameOver();
+                            break;
+                        }
+                    case "Talk to Man":
+                        {
+                            Console.WriteLine("You approach the man and attempt to initiate a meaningful conversation...\n");
+                            if (broseph > 1)
+                            {
+                                Console.WriteLine("After a few minutes of chatting about wenches and alcohol.");
+                                Console.WriteLine("He then passes out and falls to the ground. A key falls from his pocket. You take it.");
+                                //Shit happens. Aw yiss
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("He scoffs at your scrawny form and turns his attention to something in the room that isn't as puny as you.\n");
+                                //Nothing happens. Must be more bro-like
+                                break;
+                            }
+                        }
+                    case "Drink with Man":
+                        {
+                            Console.WriteLine("You walk up to the bar and order a drink for the man with the fine,\n sculpted muscles. He gives you a heavy pat on the back and thanks you.");
+                            broseph+=1;
+                            //You become bros
+                            break;
+                        }
+
+                    case "Leave":
+                        {
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine("Thy does not compute. Try again, fool!");
+                            break;
+                        }
+                }
+            }
+
+
         }
 
         private void LookAround()
